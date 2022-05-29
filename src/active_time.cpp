962a1b017297631f9ad81fa35bbae22c21fc4101
setudix <bsse1230@iit.du.ac.bd>
2022-05-30 00:20:34 +0600

    added active time = on time - idletime

--- src/active_time.cpp
+++ src/active_time.cpp
@@ -36,6 +36,21 @@ namespace SPL
             file.close();
         }
     }
+
+    int getIdleTime()
+    {
+        std::fstream file;
+        std::string filename = SPL::runCommand("pwd", "r") + "/logs/idletime/" + SPL::runCommand(SPL::Util::getDateIn_YYYY_DD_MM(),"r") + ".dat";
+
+        file.open(filename, std::ios::in);
+
+        int idletime = 0;
+        if (file.is_open()){
+            file >> idletime;
+            file.close();
+        }
+        return idletime;
+    }
     void calculatePcOnTime()
     {
         SPL::Time tempTime;
@@ -48,6 +63,16 @@ namespace SPL
             tempTime.setTime(5);
             pcOnDuration = pcOnDuration + tempTime;
             writePcOnDuration();
+            tempTime.setTime(getIdleTime());
+
+            printf("Total On Time: ");
+            pcOnDuration.displayTime();
+            printf(" || Total Idle Time : ");
+            tempTime.displayTime();
+            tempTime = pcOnDuration - tempTime;
+            printf(" || Total active Time : ");
+            tempTime.displayTime();
+            puts("");
         }
     }
 }
